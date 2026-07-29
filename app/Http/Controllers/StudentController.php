4852f9c6583548fd4c0165904ce1629a4e1441ce
Jopannnn <jov132009@gmail.com>
2026-07-29 13:50:27 +0700

    feat: implementing view

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -8,28 +8,61 @@ class StudentController extends Controller
 {
     public function index()
     {
-        return "Menampilkan halaman daftar siswa";
+        $title = 'Sistem Sekolah - Daftar Siswa';
+        $students = [
+            [
+                'id' => 1,
+                'nis' => '2024001',
+                'name' => 'digga',
+                'class' => 'XII TKJ 3',
+                'major' => 'TKJ'
+                
+                
+            ],
+
+            [
+                'id' => 2,
+                'nis' => '2024002',
+                'name' => 'Rizky',
+                'class' => 'XII AKL',
+                'major' => 'AKL'
+            ]
+        ];
+
+        return view('students.index', [
+            'title' => $title,
+            'students' => $students
+        ]);
     }
 
     public function show($id)
     {
-        return "Menampilkan siswa dengan ID: {$id}";
+        $title = 'Sistem Sekolah - Detail Siswa';
+        return view('students.show', [
+            'title' => $title
+        ]);
     }
 
     public function create()
     {
-        return "Menampilkan halaman tambah siswa";
+        $title = 'Sistem Sekolah - Tambah Siswa';
+        return view('students.create', [
+            'title' => $title
+        ]);
     }
 
     public function store(Request $request)
     {
         // Logika untuk menyimpan data siswa baru
         return "Melakukan penambahan data siswa";
-    }   
+    }
 
     public function edit($id)
     {
-        return "Menampilkan halaman edit siswa";
+        $title = 'Sistem Sekolah - Edit Siswa';
+        return view('students.edit', [
+            'title' => $title
+        ]);
     }
 
     public function update(Request $request, $id)
